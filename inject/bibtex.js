f522f15a1179a2c429d7930b50d9877ad866dabc
Luis4raujo <luisaraujo.ifba@gmail>
2016-09-22 23:30:50 -0300

    Quarto Commit - versão 01

--- inject/bibtex.js
+++ inject/bibtex.js
@@ -1,19 +1,32 @@
 currentContents = "";
 elements = 0;
 abortar = false;
-/*
-$(document).on( "keydown", function( event ) {
-     if(event.which == 66)
 
-});
-*/
 $(document).ready(function(){
     $("#gs_hdr_tsb").after("<button type='button' id='getbibtex' name='btnGB' aria-label='Get BibTex' class='gs_btnG gs_in_ib gs_btn_act gs_btn_eml'>Get BibTex</button>");
 
     $(document).on('click', '#getbibtex', function(){
         htmlParse();
     });
+
+    $(document).on('click', '#btn_close_erro', function(){
+        $("#modal-erro-bibtex").remove();
+    });
+
+
 })
+
+function createModalErro(){
+    if($("#modal-erro-bibtex").length == 0)
+    $("body").append('<div id="modal-erro-bibtex">' +
+        '<span class="gs_ico" id="btn_close_erro" style="float:right; top: 2px; background-position: -66px 0px; "></span>'+
+        '<span id="t-bibtex"><span class="gs_ico" id="erro_ico" style="position: relative; top: 2px; margin-left:14px; background-position: -22px 0px;"></span>' +
+        '<span>Occurred error in geting datas</span></span>' +
+        '<span id="m-bibtex">Possibly the Google Scholar rejected your requisition. ' +
+        'Try to click in <i>Import into Bibtex</i> of any result and answer the captcha box for release  the requisitions.</span>'+
+        '<a href="https://github.com/LuisAraujo/PluginChromeBibTex" id="l-bibtex">Look more</a>' +
+        '</div>');
+};
 
 function setElements(sinal){
     if(sinal == "+"){
@@ -37,7 +50,7 @@ function htmlParse(){
 		a = gs_fl.find("a");
 		
 		for(j=0; j < a.length; j++){
-            if($(a[j]).html() == "Importe para o BibTeX"){
+            if(($(a[j]).html() == "Importe para o BibTeX") || ($(a[j]).html() == "Import into BibTeX")){
                 setElements("+");
 				getData( $(a[j]).attr("href") );
 		   }
@@ -118,7 +131,7 @@ function sucessGetData(param){
 function createFile(){
 
     if(abortar){
-        alert("Erro ao obter os dados!");
+        createModalErro();
         return;
     }
 
